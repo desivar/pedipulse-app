16533295cf4fa741ac27f1e74f677d3077c89754
desivar <jillian_desire@yahoo.es>
2026-02-27 18:38:11 -0600

    updated types

--- client/src/types.ts
+++ client/src/types.ts
@@ -12,5 +12,5 @@ export interface Sticker {
 export interface AppData {
   message: string;
   quote: string;
-  palette: string[];
+  palette: string[]; // Note: Your backend sends a 'palette' array, so we must include it!
 }

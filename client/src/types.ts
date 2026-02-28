ed333935b48e838b3f4fbbdf3400fb8a1df91b9e
desivar <jillian_desire@yahoo.es>
2026-02-27 21:24:48 -0600

    upadte types

---
+++ client/src/types.ts
@@ -0,0 +1,15 @@
+export interface Task{
+  id:number;
+  text:string;
+}
+export interface Sticker {
+  icon: string;
+  goal: number;
+  label: string;
+}
+
+export interface AppData {
+  message: string;
+  quote: string;
+  palette: string[];
+}
